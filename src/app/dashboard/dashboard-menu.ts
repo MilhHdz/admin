4d8f2eedfea475af9cff3d4ebe4b5ce5bc1557c4
MilhHdz <devmilton98@gmail.com>
2021-08-10 00:19:30 -0500

    Ajustes en el menu de opciones

--- src/app/dashboard/dashboard-menu.ts
+++ src/app/dashboard/dashboard-menu.ts
@@ -8,24 +8,24 @@ export const MENU_ITEMS: NbMenuItem[] = [
         home: true,
     },
     {
-        title: 'Categorías',
-        icon: { icon: 'list-ul', pack: 'fas' },
-        link: '/dashboard/categorias',
-    },
-    {
-        title: 'Armadoras',
-        icon: { icon: 'cog', pack: 'fas' },
-        link: '/dashboard/armadoras',
-    },
-    {
-        title: 'Lineas',
-        icon: { icon: 'tag', pack: 'fas' },
-        link: '/dashboard/lineas',
-    },
-    {
         title: 'Clientes',
         icon: { icon: 'user', pack: 'fas' },
         link: '/dashboard/clientes',
+    },
+    {
+        title: 'Personal',
+        icon: { icon: 'people-carry', pack: 'fas' },
+        link: '/dashboard/vendedor'
+    },
+    {
+        title: 'Cotización',
+        icon: { icon: 'sticky-note', pack: 'fas' },
+        link: '/dashboard/cotizacion'
+    },
+    {
+        title: 'Caja',
+        icon: { icon: 'cash-register', pack: 'fas' },
+        link: '/dashboard/ventas'
     },
     {
         title: 'Sagaji',
@@ -33,13 +33,35 @@ export const MENU_ITEMS: NbMenuItem[] = [
         link: '/dashboard/sagaji',
     },
     {
-        title: 'Proveedores',
-        icon: { icon: 'parachute-box', pack: 'fas' },
-        link: '/dashboard/proveedores',
-    },
-    {
-        title: 'Facturas',
-        icon: { icon: 'file-invoice', pack: 'fas' },
-        link: '/dashboard/facturas',
+        title: 'Más herramientas',
+        expanded: true,
+        icon: { icon: 'clipboard', pack: 'fas'},
+        children: [
+            {
+                title: 'Categorías',
+                icon: { icon: 'list-ul', pack: 'fas' },
+                link: '/dashboard/categorias',
+            },
+            {
+                title: 'Armadoras',
+                icon: { icon: 'cog', pack: 'fas' },
+                link: '/dashboard/armadoras',
+            },
+            {
+                title: 'Lineas',
+                icon: { icon: 'tag', pack: 'fas' },
+                link: '/dashboard/lineas',
+            },
+            {
+                title: 'Proveedores',
+                icon: { icon: 'parachute-box', pack: 'fas' },
+                link: '/dashboard/proveedores',
+            },
+            {
+                title: 'Facturas',
+                icon: { icon: 'file-invoice', pack: 'fas' },
+                link: '/dashboard/facturas',
+            },
+        ]
     },
 ];
